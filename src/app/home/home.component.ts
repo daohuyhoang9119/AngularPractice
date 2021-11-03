37a49e05c2e9ba3da76f78e32d5d875f3f913f13
HuyHoang <daohuyhoang9119@gmail.com>
2021-11-04 01:21:11 +0700

    Combobox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   constructor() {}
+  public districts: string[] = [];
 
   public listOfFruits = [
     'orange',
@@ -47,7 +49,67 @@ export class HomeComponent implements OnInit {
       sale: true,
     },
   ];
+  public VietNam = [
+    { city: 'Chon Thanh Pho', district: [] },
+    {
+      city: 'An Giang',
+      district: [
+        'Thành phố Long Xuyên',
+        'Thành phố Châu Đốc',
+        'Thị xã Tân Châu',
+        'Huyện An Phú',
+        'Huyện Châu Phú',
+        'Huyện Châu Thành',
+        'Huyện Chợ Mới',
+        'Huyện Phú Tân',
+        'Huyện Thoại Sơn',
+        'Huyện Tịnh Biên',
+        'Huyện Tri Tôn',
+      ],
+    },
+    {
+      city: 'Bà Rịa - Vũng Tàu',
+      district: [
+        'Thành phố Vũng Tàu',
+        'Thị xã Bà Rịa',
+        'Thị xã Phú Mỹ',
+        'Huyện Châu Đức',
+        'Huyện Côn Đảo',
+        'Huyện Đất Đỏ',
+        'Huyện Long Điền',
+        'Huyện Tân Thành',
+        'Huyện Xuyên Mộc',
+      ],
+    },
+    {
+      city: 'Bạc Liêu',
+      district: [
+        'Thành phố Bạc Liêu',
+        'Huyện Đông Hải',
+        'Huyện Giá Rai',
+        'Huyện Hòa Bình',
+        'Huyện Hồng Dân',
+        'Huyện Phước Long',
+        'Huyện Vĩnh Lợi',
+      ],
+    },
+  ];
   ngOnInit(): void {
-    console.log(`fruit: ${this.listOfFruits}`);
+    console.log('cities', this.VietNam);
+  }
+
+  public changeCity(event: any): void {
+    console.log('event', event);
+    const city = event.target.value;
+    console.log(city);
+    if (!city) {
+      return;
+    }
+    //c1
+    const search = this.VietNam.filter((data) => data.city === city);
+    console.log('search', search);
+    if (search && search.length > 0) {
+      this.districts = search[0].district;
+    }
   }
 }
